7cb8d507c839eb1716fca8957c56dc9e3a649f82
HubertHuckevoll <konstantin.meyer@gmail.com>
2024-10-27 15:25:44 +0100

    adding Amtsgericht

--- view/generic/php.page/talVP.php
+++ view/generic/php.page/talVP.php
@@ -32,10 +32,9 @@ class talVP extends \cb\view\page\cbPageVP
                                                'Location, Locationdesign, Regie, Regieturm">'.
                 '<meta name="description" content="'.htmlentities($this->data['metaDescription']).'">'.
                 '<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">'.
-                '<meta name="apple-mobile-web-app-capable" content="yes">'.
+                '<meta name="mobile-web-app-capable" content="yes">'.
                 '<meta name="apple-mobile-web-app-status-bar-style" content="black">'.
                 '<link rel="shortcut icon" href="favicon.ico">'.
-                '<!--link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous"-->'.
                 '<link rel="stylesheet" type="text/css" href="'.CB_CSS_ROOT.'cbBox.css'.'">'.
                 '<link rel="stylesheet" type="text/css" href="'.CB_CSS_ROOT.'cbArticleClassicS2.css'.'">'.
                 '<link rel="stylesheet" type="text/css" href="'.CB_CSS_ROOT.'cbUserClasses.css">'.
@@ -132,6 +131,7 @@ class talVP extends \cb\view\page\cbPageVP
                    '<div id="footerRight">'.
                      '<div>Geschäftsführer: Mathias Meyer</div>'.
                      '<div>USt.-IdNr. DE 23 111 9997</div>'.
+                     '<div>Amtsgericht Chemnitz HRB 18656</div>'.
                      '<div>IBAN DE81870500003557005550 &middot; BIC CHEKDE81XXX</div>'.
                      '<div><a href="TAL-pages/Impressum.html">Impressum</a></div>'.
                      '<div><a href="TAL-pages/dsgvo.html">Datenschutzerkl&auml;rung</a></div>'.
